80bf751a74d69a20ec02c66bdce41759521f0062
Stephen Sylvia <ssylvia@esri.com>
2012-10-12 10:51:54 -0400

    php write

--- export.php
+++ export.php
@@ -1,29 +1,10 @@
-<?php
-$filename = 'test.txt';
-$somecontent = "Add this to the file\n";
-
-// Let's make sure the file exists and is writable first.
-if (is_writable($filename)) {
-
-    // In our example we're opening $filename in append mode.
-    // The file pointer is at the bottom of the file hence
-    // that's where $somecontent will go when we fwrite() it.
-    if (!$handle = fopen($filename, 'a')) {
-         echo "Cannot open file ($filename)";
-         exit;
-    }
-
-    // Write $somecontent to our opened file.
-    if (fwrite($handle, $somecontent) === FALSE) {
-        echo "Cannot write to file ($filename)";
-        exit;
-    }
-
-    echo "Success, wrote ($somecontent) to file ($filename)";
-
-    fclose($handle);
-
-} else {
-    echo "The file $filename is not writable";
-}
-?>
+<?php 
+ $File = "YourFile.txt"; 
+ $Handle = fopen($File, 'w');
+ $Data = "Jane Doe\n"; 
+ fwrite($Handle, $Data); 
+ $Data = "Bilbo Jones\n"; 
+ fwrite($Handle, $Data); 
+ print "Data Written"; 
+ fclose($Handle); 
+ ?>
