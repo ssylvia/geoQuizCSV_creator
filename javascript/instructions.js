ec02d2b164124c507cc5d61634785b403affe861
Stephen Sylvia <ssylvia@esri.com>
2012-11-09 16:56:25 -0500

    first run

--- javascript/instructions.js
+++ javascript/instructions.js
@@ -17,3 +17,12 @@ var resetLayout = function(){
     $("#bannerArt").css("margin-left",($("#content").width()-406)/2);
     $(".screenshot").width($("#instuctionsWrapper").width()-6);
 };
+
+var launchApp = function(){
+    if ($("#webmap").val() !== ""){
+        window.open = "http://storymaps.esri.com/stories/2012/treasure-hunt/?webmap="+$("#webmap").val();
+    }
+    else{
+        alert("You must first provide a webmap ID in the text box above.");
+    }
+}
